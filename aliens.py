28a341daf887318124439ec0d53593d8665c7bc6
Fee <felicity124martin@gmail.com>
2026-02-15 23:55:48 -0500

    5-4 pr

--- aliens.py
+++ aliens.py
@@ -4,4 +4,6 @@ alien_color = input("What color is the alien that you killed")
 
 if alien_color == "green":
     print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
     
