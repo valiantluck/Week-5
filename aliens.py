7d91c744424aace16b6694cfffffd75b3ef52dd6
valiantluck <felicity124martin@gmail.com>
2026-02-17 10:02:15 -0500

    Revert "pr 5-5 (#4)"
    This reverts commit 391a4a37159c65243cd865a30bc6b7a613276ffb.

--- aliens.py
+++ aliens.py
@@ -4,11 +4,4 @@ alien_color = input("What color is the alien that you killed")
 
 if alien_color == "green":
     print("You just earned 5 points!")
-elif alien_color == "yellow":
-    print("you just earned 10 points")
-elif alien_color == "red":
-    print("you just earned 15 points")
-else:
-    print("you just earned 0 points")
-    
     
